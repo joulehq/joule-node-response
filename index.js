eb3d9fd4ce95871c4f225907c03102750bde79c0
Jaisen Mathai <jaisen@jmathai.com>
2016-02-01 00:24:22 -0800

    Change internal variable names from payload and data to result

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
 var _container = {
 	code: null,
 	message: null,
-	data: null
+	result: null
 };
 
-var success = function(context, payload) {
-	_container.data = payload;
+var success = function(context, result) {
+	_container.result = result;
 	context.succeed(JSON.stringify(_container));
 };
 
-var error = function(context, payload) {
-	_container.data = payload;
+var error = function(context, result) {
+	_container.result = result;
 	context.fail(JSON.stringify(_container));
 };
 
 module.exports = {
   // success codes
-  success200 : function(context, payload) {
+  success200 : function(context, result) {
     _container.code = 200;
     _container.message = "200: Ok";
-    success(context, payload);
+    success(context, result);
   },
 
-  success201 : function(context, payload) {
+  success201 : function(context, result) {
     _container.code = 201;
     _container.message = "201: Created";
-    success(context, payload);
+    success(context, result);
   },
 
-  success204 : function(context, payload) {
+  success204 : function(context, result) {
     _container.code = 204;
     _container.message = "204: No Content";
-    success(context, payload);
+    success(context, result);
   },
 
   // error codes
-  error400 : function(context, payload) {
+  error400 : function(context, result) {
     _container.code = 400;
     _container.message = "400: Bad Request";
-    error(context, payload);
+    error(context, result);
   },
 
-  error401 : function(context, payload) {
+  error401 : function(context, result) {
     _container.code = 401;
     _container.message = "401: Unauthorized";
-    error(context, payload);
+    error(context, result);
   },
 
-  error404 : function(context, payload) {
+  error404 : function(context, result) {
     _container.code = 404;
     _container.message = "404: Not Found";
-    error(context, payload);
+    error(context, result);
   },
 
-  error500 : function(context, payload) {
+  error500 : function(context, result) {
     _container.code = 500;
     _container.message = "500: Internal Server Error";
-    error(context, payload);
+    error(context, result);
   }
 };
